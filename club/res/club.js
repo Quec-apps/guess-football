36e44b543bae830279d23e18f205f152ee870d1d
Quec-apps <79459289+Quec-apps@users.noreply.github.com>
2023-10-30 12:40:10 +0530

    Add files via upload

--- club/res/club.js
+++ club/res/club.js
@@ -1,108 +1,310 @@
 a = 0;
 a++;
-window["ans"+a] = "FC Barcelona"; //Ans1
-window["noans1"+a] = "Chelsea FC";
-window["noans2"+a] = "AS Roma";
-window["noans3"+a] = "RSC Anderlecht";
-window["noans4"+a] = "Club Libertad";
+window["ans"+a] = "Manchester City"; //Ans1
+window["noans1"+a] = "Fiorentina";
+window["noans2"+a] = "Paris Saint-Germain";
+window["noans3"+a] = "River Plate";
+window["noans4"+a] = "Newcastle United";
 
 a++;
-window["ans"+a] = "FC Internazionale Milano"; //Ans2
-window["noans1"+a] = "Estudiantes De La Plata";
-window["noans2"+a] = "FC Bayern Munich";
-window["noans3"+a] = "FC Bayern Munich";
-window["noans4"+a] = "Valencia CF";
+window["ans"+a] = "Bayern München"; //Ans2
+window["noans1"+a] = "Lazio";
+window["noans2"+a] = "Inter Milan";
+window["noans3"+a] = "Lens";
+window["noans4"+a] = "Real Betis";
 
 a++;
-window["ans"+a] = "FC Bayern Munich"; //Ans3
-window["noans1"+a] = "AS Roma";
-window["noans2"+a] = "Estudiantes De La Plata";
-window["noans3"+a] = "Club Atl�tico De Madrid";
-window["noans4"+a] = "Sport Club Internacional";
+window["ans"+a] = "Inter Milan"; //Ans3
+window["noans1"+a] = "Bayern München";
+window["noans2"+a] = "Braga";
+window["noans3"+a] = "Red Bull Salzburg";
+window["noans4"+a] = "Shakhtar Donetsk";
 
 a++;
-window["ans"+a] = "Estudiantes De La Plata"; //Ans4
-window["noans1"+a] = "Valencia CF";
-window["noans2"+a] = "Valencia CF";
-window["noans3"+a] = "FC Internazionale Milano";
-window["noans4"+a] = "Cruzeiro Esporte Clube";
+window["ans"+a] = "Liverpool FC"; //Ans4
+window["noans1"+a] = "Manchester City";
+window["noans2"+a] = "Paris Saint-Germain";
+window["noans3"+a] = "Sporting";
+window["noans4"+a] = "Tottenham Hotspur";
 
 a++;
-window["ans"+a] = "Club Atl�tico De Madrid"; //Ans5
-window["noans1"+a] = "AS Roma";
-window["noans2"+a] = "Valencia CF";
+window["ans"+a] = "FC Barcelona"; //Ans5
+window["noans1"+a] = "Sporting";
+window["noans2"+a] = "Flamengo";
+window["noans3"+a] = "Sparta Prague";
+window["noans4"+a] = "Benfica";
+
+a++;
+window["ans"+a] = "Atlético Madrid"; //Ans6
+window["noans1"+a] = "Manchester City";
+window["noans2"+a] = "Bayer Leverkusen";
+window["noans3"+a] = "Slavia Prague";
+window["noans4"+a] = "Sporting";
+
+a++;
+window["ans"+a] = "Arsenal"; //Ans7
+window["noans1"+a] = "Shakhtar Donetsk";
+window["noans2"+a] = "Tottenham Hotspur";
+window["noans3"+a] = "Shakhtar Donetsk";
+window["noans4"+a] = "River Plate";
+
+a++;
+window["ans"+a] = "SSC Napoli"; //Ans8
+window["noans1"+a] = "Slavia Prague";
+window["noans2"+a] = "Real Betis";
+window["noans3"+a] = "AC Milan";
+window["noans4"+a] = "Arsenal";
+
+a++;
+window["ans"+a] = "Borussia Dortmund"; //Ans9
+window["noans1"+a] = "Fiorentina";
+window["noans2"+a] = "Newcastle United";
+window["noans3"+a] = "Flamengo";
+window["noans4"+a] = "Manchester City";
+
+a++;
+window["ans"+a] = "FC Porto"; //Ans10
+window["noans1"+a] = "Lens";
+window["noans2"+a] = "VfB Stuttgart";
+window["noans3"+a] = "River Plate";
+window["noans4"+a] = "Benfica";
+
+a++;
+window["ans"+a] = "Newcastle United"; //Ans11
+window["noans1"+a] = "Atalanta";
+window["noans2"+a] = "Lens";
+window["noans3"+a] = "Paris Saint-Germain";
+window["noans4"+a] = "Shakhtar Donetsk";
+
+a++;
+window["ans"+a] = "AC Milan"; //Ans12
+window["noans1"+a] = "Roma";
+window["noans2"+a] = "Benfica";
+window["noans3"+a] = "Feyenoord";
+window["noans4"+a] = "Arsenal";
+
+a++;
+window["ans"+a] = "Bayer Leverkusen"; //Ans13
+window["noans1"+a] = "Sparta Prague";
+window["noans2"+a] = "Aston Villa";
+window["noans3"+a] = "Bologna";
+window["noans4"+a] = "Feyenoord";
+
+a++;
+window["ans"+a] = "Real Sociedad"; //Ans14
+window["noans1"+a] = "Botafogo FC";
+window["noans2"+a] = "Flamengo";
+window["noans3"+a] = "Atalanta";
+window["noans4"+a] = "Lazio";
+
+a++;
+window["ans"+a] = "Aston Villa"; //Ans15
+window["noans1"+a] = "River Plate";
+window["noans2"+a] = "Fiorentina";
+window["noans3"+a] = "Flora Tallinn";
+window["noans4"+a] = "FC Porto";
+
+a++;
+window["ans"+a] = "Benfica"; //Ans16
+window["noans1"+a] = "Feyenoord";
+window["noans2"+a] = "Flora Tallinn";
+window["noans3"+a] = "Flamengo";
+window["noans4"+a] = "Slavia Prague";
+
+a++;
+window["ans"+a] = "Paris Saint-Germain"; //Ans17
+window["noans1"+a] = "Liverpool FC";
+window["noans2"+a] = "Flamengo";
 window["noans3"+a] = "Liverpool FC";
-window["noans4"+a] = "AS Roma";
+window["noans4"+a] = "Manchester United";
 
 a++;
-window["ans"+a] = "Manchester United FC"; //Ans6
-window["noans1"+a] = "Valencia CF";
-window["noans2"+a] = "FC Bayern Munich";
-window["noans3"+a] = "FC Bayern Munich";
+window["ans"+a] = "Tottenham Hotspur"; //Ans18
+window["noans1"+a] = "Fiorentina";
+window["noans2"+a] = "Liverpool FC";
+window["noans3"+a] = "Real Betis";
+window["noans4"+a] = "Manchester City";
+
+a++;
+window["ans"+a] = "Juventus"; //Ans19
+window["noans1"+a] = "Paris Saint-Germain";
+window["noans2"+a] = "FC Barcelona";
+window["noans3"+a] = "PSV Eindhoven";
+window["noans4"+a] = "Palmeiras";
+
+a++;
+window["ans"+a] = "Feyenoord"; //Ans20
+window["noans1"+a] = "Juventus";
+window["noans2"+a] = "Manchester United";
+window["noans3"+a] = "Bayer Leverkusen";
+window["noans4"+a] = "Juventus";
+
+a++;
+window["ans"+a] = "Lazio"; //Ans21
+window["noans1"+a] = "Red Bull Salzburg";
+window["noans2"+a] = "Slavia Prague";
+window["noans3"+a] = "AC Milan";
+window["noans4"+a] = "Red Bull Salzburg";
+
+a++;
+window["ans"+a] = "PSV Eindhoven"; //Ans22
+window["noans1"+a] = "SSC Napoli";
+window["noans2"+a] = "Galatasaray";
+window["noans3"+a] = "River Plate";
+window["noans4"+a] = "Sporting";
+
+a++;
+window["ans"+a] = "Sporting"; //Ans23
+window["noans1"+a] = "Borussia Dortmund";
+window["noans2"+a] = "Aston Villa";
+window["noans3"+a] = "SSC Napoli";
+window["noans4"+a] = "Roma";
+
+a++;
+window["ans"+a] = "Fiorentina"; //Ans24
+window["noans1"+a] = "River Plate";
+window["noans2"+a] = "Liverpool FC";
+window["noans3"+a] = "Sporting";
+window["noans4"+a] = "Slavia Prague";
+
+a++;
+window["ans"+a] = "Atalanta"; //Ans25
+window["noans1"+a] = "Roma";
+window["noans2"+a] = "Bologna";
+window["noans3"+a] = "SSC Napoli";
+window["noans4"+a] = "Feyenoord";
+
+a++;
+window["ans"+a] = "Red Bull Salzburg"; //Ans26
+window["noans1"+a] = "AC Milan";
+window["noans2"+a] = "Manchester City";
+window["noans3"+a] = "Manchester City";
+window["noans4"+a] = "AC Milan";
+
+a++;
+window["ans"+a] = "Manchester United"; //Ans27
+window["noans1"+a] = "Slavia Prague";
+window["noans2"+a] = "Lens";
+window["noans3"+a] = "River Plate";
+window["noans4"+a] = "Atlético Madrid";
+
+a++;
+window["ans"+a] = "Slavia Prague"; //Ans28
+window["noans1"+a] = "Bologna";
+window["noans2"+a] = "Braga";
+window["noans3"+a] = "Fiorentina";
+window["noans4"+a] = "Flamengo";
+
+a++;
+window["ans"+a] = "River Plate"; //Ans29
+window["noans1"+a] = "Atlético Madrid";
+window["noans2"+a] = "Lens";
+window["noans3"+a] = "Liverpool FC";
+window["noans4"+a] = "Braga";
+
+a++;
+window["ans"+a] = "VfB Stuttgart"; //Ans30
+window["noans1"+a] = "Shakhtar Donetsk";
+window["noans2"+a] = "Lens";
+window["noans3"+a] = "Manchester City";
+window["noans4"+a] = "Bayer Leverkusen";
+
+a++;
+window["ans"+a] = "Palmeiras"; //Ans31
+window["noans1"+a] = "Atalanta";
+window["noans2"+a] = "SSC Napoli";
+window["noans3"+a] = "Sparta Prague";
+window["noans4"+a] = "Lazio";
+
+a++;
+window["ans"+a] = "Red Star Belgrade"; //Ans32
+window["noans1"+a] = "Manchester United";
+window["noans2"+a] = "Sparta Prague";
+window["noans3"+a] = "Newcastle United";
 window["noans4"+a] = "Liverpool FC";
 
 a++;
-window["ans"+a] = "Chelsea FC"; //Ans7
-window["noans1"+a] = "Real Madrid CF";
-window["noans2"+a] = "Valencia CF";
-window["noans3"+a] = "FC Internazionale Milano";
-window["noans4"+a] = "Estudiantes De La Plata";
-
-a++;
-window["ans"+a] = "Liverpool FC"; //Ans8
-window["noans1"+a] = "FC Bayern Munich";
-window["noans2"+a] = "Club Libertad";
-window["noans3"+a] = "Club Libertad";
-window["noans4"+a] = "Club Libertad";
-
-a++;
-window["ans"+a] = "Real Madrid CF"; //Ans9
-window["noans1"+a] = "Club Libertad";
-window["noans2"+a] = "FC Barcelona";
+window["ans"+a] = "Lens"; //Ans33
+window["noans1"+a] = "AC Milan";
+window["noans2"+a] = "Fiorentina";
 window["noans3"+a] = "FC Barcelona";
-window["noans4"+a] = "Estudiantes De La Plata";
+window["noans4"+a] = "Bayer Leverkusen";
 
 a++;
-window["ans"+a] = "Sport Club Internacional"; //Ans10
-window["noans1"+a] = "Valencia CF";
-window["noans2"+a] = "Chelsea FC";
-window["noans3"+a] = "Cruzeiro Esporte Clube";
-window["noans4"+a] = "Chelsea FC";
+window["ans"+a] = "Braga"; //Ans34
+window["noans1"+a] = "Flora Tallinn";
+window["noans2"+a] = "FC Porto";
+window["noans3"+a] = "Red Star Belgrade";
+window["noans4"+a] = "Feyenoord";
 
 a++;
-window["ans"+a] = "AS Roma"; //Ans11
-window["noans1"+a] = "RSC Anderlecht";
-window["noans2"+a] = "Sport Club Internacional";
-window["noans3"+a] = "Chelsea FC";
-window["noans4"+a] = "Sport Club Internacional";
+window["ans"+a] = "Roma"; //Ans35
+window["noans1"+a] = "Juventus";
+window["noans2"+a] = "Inter Milan";
+window["noans3"+a] = "River Plate";
+window["noans4"+a] = "Tottenham Hotspur";
 
 a++;
-window["ans"+a] = "RSC Anderlecht"; //Ans12
-window["noans1"+a] = "Cruzeiro Esporte Clube";
-window["noans2"+a] = "Cruzeiro Esporte Clube";
-window["noans3"+a] = "FC Bayern Munich";
-window["noans4"+a] = "FC Barcelona";
+window["ans"+a] = "West Ham United"; //Ans36
+window["noans1"+a] = "Feyenoord";
+window["noans2"+a] = "SSC Napoli";
+window["noans3"+a] = "Roma";
+window["noans4"+a] = "PSV Eindhoven";
 
 a++;
-window["ans"+a] = "Valencia CF"; //Ans13
-window["noans1"+a] = "Sport Club Internacional";
-window["noans2"+a] = "Liverpool FC";
-window["noans3"+a] = "FC Barcelona";
-window["noans4"+a] = "Real Madrid CF";
+window["ans"+a] = "Flora Tallinn"; //Ans37
+window["noans1"+a] = "Lens";
+window["noans2"+a] = "Botafogo FC";
+window["noans3"+a] = "Bayern München";
+window["noans4"+a] = "Palmeiras";
 
 a++;
-window["ans"+a] = "Cruzeiro Esporte Clube"; //Ans14
-window["noans1"+a] = "Valencia CF";
-window["noans2"+a] = "Manchester United FC";
-window["noans3"+a] = "Valencia CF";
-window["noans4"+a] = "FC Barcelona";
+window["ans"+a] = "Sparta Prague"; //Ans38
+window["noans1"+a] = "Paris Saint-Germain";
+window["noans2"+a] = "Flora Tallinn";
+window["noans3"+a] = "Manchester United";
+window["noans4"+a] = "Juventus";
 
 a++;
-window["ans"+a] = "Club Libertad"; //Ans15
-window["noans1"+a] = "FC Bayern Munich";
-window["noans2"+a] = "Liverpool FC";
-window["noans3"+a] = "AS Roma";
-window["noans4"+a] = "Club Atl�tico De Madrid";
+window["ans"+a] = "Galatasaray"; //Ans39
+window["noans1"+a] = "Red Bull Salzburg";
+window["noans2"+a] = "Shakhtar Donetsk";
+window["noans3"+a] = "Shakhtar Donetsk";
+window["noans4"+a] = "Botafogo FC";
 
+a++;
+window["ans"+a] = "Bologna"; //Ans40
+window["noans1"+a] = "Lazio";
+window["noans2"+a] = "Galatasaray";
+window["noans3"+a] = "Liverpool FC";
+window["noans4"+a] = "Lens";
+
+a++;
+window["ans"+a] = "Botafogo FC"; //Ans41
+window["noans1"+a] = "Arsenal";
+window["noans2"+a] = "Red Bull Salzburg";
+window["noans3"+a] = "Tottenham Hotspur";
+window["noans4"+a] = "PSV Eindhoven";
+
+a++;
+window["ans"+a] = "Flamengo"; //Ans42
+window["noans1"+a] = "Atalanta";
+window["noans2"+a] = "Shakhtar Donetsk";
+window["noans3"+a] = "SSC Napoli";
+window["noans4"+a] = "Red Bull Salzburg";
+
+a++;
+window["ans"+a] = "Shakhtar Donetsk"; //Ans43
+window["noans1"+a] = "Galatasaray";
+window["noans2"+a] = "Roma";
+window["noans3"+a] = "Flora Tallinn";
+window["noans4"+a] = "Palmeiras";
+
+a++;
+window["ans"+a] = "Real Betis"; //Ans44
+window["noans1"+a] = "Atlético Madrid";
+window["noans2"+a] = "Atlético Madrid";
+window["noans3"+a] = "Red Bull Salzburg";
+window["noans4"+a] = "Atlético Madrid";
 
 foot_player_LevelTotal = a;
